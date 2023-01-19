7662219de504592aa3132af13167428bde403d27
Maksim Shelepov <graf_ice@mail.ru>
2023-01-19 09:11:43 +0300

    278. First Bad Version

---
+++ src/main/java/ru/maxol/problems/P278.java
@@ -0,0 +1,32 @@
+package ru.maxol.problems;
+
+/***
+ * 278. First Bad Version
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ * You are given an API bool isBadVersion(version) which returns whether version is bad.
+ * Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ */
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class P278 {
+
+    public static int firstBadVersion(int n) {
+        int min = 1;
+        int max = n;
+
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            if (isBadVersion(mid)) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return max;
+    }
+}
