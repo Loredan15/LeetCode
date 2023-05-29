6828223b21e21ef44383548d0b35f893dfa41c53
Maksim Shelepov <graf_ice@mail.ru>
2023-05-29 15:37:30 +0300

    43. Multiply Strings

--- src/test/java/ru/maxol/problems/P43Test.java
+++ src/test/java/ru/maxol/problems/P43Test.java
@@ -1,7 +1,14 @@
 package ru.maxol.problems;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class P43Test {
+
+    @Test
+    public void case1(){
+
+    }
 
 }
